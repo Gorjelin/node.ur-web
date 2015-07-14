7ba234bf942870394ef66fad9d98e63bb0853091
Gorjelin <ev.yg@mail.ru>
2015-07-14 12:44:09 +0000

    ver 0.3 all moduls added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-var http = require('http');http.createServer(function (req, res) {  res.writeHead(200, {'Content-Type': 'text/plain'});  res.end('Тест \n');}).listen(8001, '0.0.0.0');console.log('Server running at http://0.0.0.1:8001/');
+'use strict';
+
+var http = require('http'),
+	express = require('express');
+	
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('Привет!');
+});
+
+http.createServer(app).listen(8001, function(){
+	console.log('yello');
+})
